0c52755ff0fd13b367df530e412e8e4b8617be9d
brave-aditya <pratapaditya84@gmail.com>
2020-08-22 11:13:46 +0530

    Updates

---
+++ precache-manifest.1fcc053100f1dbcc75e1686cf16981dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c4b2b1f88ddf5a1c40ec2591542849",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "135a501619fd5dad8a65",
+    "url": "/weather/static/css/main.b7c66f6b.chunk.css"
+  },
+  {
+    "revision": "ef4b7fe8686dd6024ca9",
+    "url": "/weather/static/js/2.3e741c71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.3e741c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135a501619fd5dad8a65",
+    "url": "/weather/static/js/main.9b965d7f.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "cd1e5382337960e52817ddbcec73baab",
+    "url": "/weather/static/media/1st.cd1e5382.jpg"
+  }
+]);
